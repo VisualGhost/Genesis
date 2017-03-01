890bf2d15a9f71257a2617fdf0856708086e3add
VisualGhost <weatherrocket@gmail.com>
2017-03-01 22:13:16 +0200

    Mocking webserver response

--- app/src/main/java/com/genesis/networking/RestClient.java
+++ app/src/main/java/com/genesis/networking/RestClient.java
@@ -5,24 +5,67 @@ import android.util.Log;
 import com.genesis.BuildConfig;
 import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 
+import java.util.concurrent.TimeUnit;
+
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+
+// https://github.com/square/okhttp/tree/master/mockwebserver
+// response.throttleBody(1024, 1, TimeUnit.SECONDS);
 
 @Singleton
 public class RestClient {
 
+    private static final String MOCK_SERVER_RESPONSE = "{\n" +
+            "  \"status\": \"OK\",\n" +
+            "  \"copyright\": \"Copyright (c) 2017 The New York Times Company.  All Rights Reserved.\",\n" +
+            "  \"num_results\": 2,\n" +
+            "  \"results\": [\n" +
+            "    {\n" +
+            "      \"title\": \"\\\"I GIVE YOU MY BODY ...\\\"\",\n" +
+            "      \"author\": \"Diana Gabaldon\"\n" +
+            "    },\n" +
+            "    {\n" +
+            "      \"title\": \"\\\"MOST BLESSED OF THE PATRIARCHS\\\"\",\n" +
+            "      \"author\": \"Annette Gordon-Reed and Peter S Onuf\"\n" +
+            "    }\n" +
+            "  ]\n" +
+            "}";
+
+    private MockWebServer mockWebServer;
+
+
     private static final String TAG = RestClient.class.getSimpleName();
 
-    private String mBaseUrl;
+    private volatile String mBaseUrl;
 
     @Inject
     public RestClient() {
-        // empty
+
+        Log.d(TAG, "::::: RestClient");
+
+        mockWebServer = new MockWebServer();
+        mockWebServer.enqueue(new MockResponse().setBody(MOCK_SERVER_RESPONSE).throttleBody(70, 1, TimeUnit.SECONDS));
+//        mockWebServer.setDispatcher(new Dispatcher() {
+//            @Override
+//            public MockResponse dispatch(RecordedRequest request) throws InterruptedException {
+//                SystemClock.sleep(5000);
+//                return new MockResponse().setBody(MOCK_SERVER_RESPONSE);
+//            }
+//        });
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                mBaseUrl = mockWebServer.url("").toString();
+            }
+        }).start();
     }
 
     public void setBaseUrl(String baseUrl) {
